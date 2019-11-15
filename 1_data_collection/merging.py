82357fe3d88561331b0e4c4af92e44b19b69ff5b
David Steiner <38961224+DavidDanielSteiner@users.noreply.github.com>
2019-11-15 23:44:42 +0100

    finish data collection

--- 1_data_collection/merging.py
+++ 1_data_collection/merging.py
@@ -4,98 +4,154 @@ Created on Mon Nov 11 18:33:32 2019
 
 @author: David
 """
-
-#https://coinmetrics.io/introducing-adjusted-estimates/
-
-#pd.set_option('display.max_columns', None)
-
 import pandas as pd
+import random
+import string
 
 #import
 btc_price_data = pd.read_csv("data/btc.csv") #https://coinmetrics.io/community-data-dictionary/
-tnx = pd.read_csv("data/transactions_500BTC_2.csv")
+btc_price_data = btc_price_data[['date', 'CapMrktCurUSD','PriceUSD']]
+tnx = pd.read_csv("data/transactions_over_500BTC.csv")
 wallets_1 = pd.read_csv("data/wallets_walletexplorer.csv", index_col=False)
 wallets_2 = pd.read_csv("data/wallets_bitinfocharts.csv", index_col=False)
-#wallets_3 = pd.read_csv("data/wallets_bitinfocharts_missing.csv", index_col=False)
-wallets_3 = pd.read_csv("data/found_full.csv", index_col=False)
+wallets_3 = pd.read_csv("data/wallets_bitinfocharts_missing.csv", index_col=False)
+wallets_4 = pd.read_csv("data/wallets_cryptoground.csv", index_col=False)
+
 
 #Preprocess transactions
 tnx['date'] = pd.to_datetime(tnx['block_timestamp']).apply(lambda x: '{year}-{month}-{day}'.format(year=x.year, month=x.month, day=x.day))
 
-#Merge with wallets
-wallets = wallets_1[['address', 'owner', 'category']]
-wallets = wallets.append(wallets_2.dropna())
-wallets = wallets.append(wallets_3.dropna())
-wallets = wallets.drop_duplicates(subset='address', keep='last')
+def merge_filter(new_wallet):
+    #Merge with wallets
+    wallets = wallets_1[['address', 'owner', 'category']]
+    wallets = wallets.append(wallets_2.dropna())
+    wallets = wallets.append(wallets_3.dropna())
+    wallets = wallets.append(wallets_4.dropna())
+    wallets = wallets.append(new_wallet)
+    wallets = wallets.drop_duplicates(subset='address', keep='last')
 
-sender = pd.merge(tnx, wallets, left_on='sender', right_on='address', how='left')
-sender.rename(columns = {"owner": "sender_name", 
-                         "category":"sender_category"
-                         }, inplace = True) 
-columns = ['address']
-sender = sender.drop(columns, axis=1)
+    sender = pd.merge(tnx, wallets, left_on='sender', right_on='address', how='left')
+    sender.rename(columns = {"owner": "sender_name", "category":"sender_category"}, inplace = True) 
+    columns = ['address']
+    sender = sender.drop(columns, axis=1)    
+    receiver = pd.merge(tnx, wallets, left_on='receiver', right_on='address', how='left')
+    receiver.rename(columns = {"owner": "receiver_name", "category":"receiver_category"}, inplace = True) 
+    columns = ['address']
+    receiver = receiver.drop(columns, axis=1)
+    
+    tnx_labeled = pd.merge(sender, receiver,  how='inner', on=['hash', 'block_timestamp', 'sender','receiver', 'date', 'value'])
+    tnx_labeled.rename(columns = {"value": "btc"}, inplace = True) 
+    
+    #Merge with price data
+    btc_price_data['date'] = pd.to_datetime(btc_price_data['date']).apply(lambda x: '{year}-{month}-{day}'.format(year=x.year, month=x.month, day=x.day))   
+    data = pd.merge(tnx_labeled, btc_price_data, on='date', how='inner')
+    data['dollar'] = data['btc'] * data['PriceUSD']
+    data['percent_marketcap'] = (data['dollar'] / data['CapMrktCurUSD']) *100
+    
+    #filter
+    filtered_transactions = data[data['dollar'] >= 10000000]
+    #filtered_transactions = data[data['percent_marketcap'] >= 0.5]
 
-receiver = pd.merge(tnx, wallets, left_on='receiver', right_on='address', how='left')
-receiver.rename(columns = {"owner": "receiver_name", 
-                         "category":"receiver_category"
-                         }, inplace = True) 
-columns = ['address']
-receiver = receiver.drop(columns, axis=1)
+    #filtered_transactions.to_csv("transactions_10MIO.csv", index=False)
+    return filtered_transactions
 
-tnx_labeled = pd.merge(sender, receiver,  how='inner', on=['hash', 'block_timestamp', 'sender','receiver', 'date', 'value'])
-tnx_labeled.rename(columns = {"value": "btc"}, inplace = True) 
+def randomString(x):
+    """Generate a random string of fixed length """
+    stringLength=30
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(stringLength))
 
-#Merge with price data
-btc_price_data = btc_price_data[['date', 'CapMrktCurUSD','PriceUSD']]
-btc_price_data['date'] = pd.to_datetime(btc_price_data['date']).apply(lambda x: '{year}-{month}-{day}'.format(year=x.year, month=x.month, day=x.day))
+def check_owner(x):    
+    if not isinstance(x, str):
+        x = sorted(x, key=len)
+        x = x[0]
+    
+    length = len(x)
+    if length == 30:
+        return 'unknown'
+    else:
+        return x
 
-data = pd.merge(tnx_labeled, btc_price_data, on='date', how='inner')
-#data.set_index('date', inplace=True)
-#data = data.sort_values('date', axis=0, ascending=True)
-data['dollar'] = data['btc'] * data['PriceUSD']
-data['percent_marketcap'] = (data['dollar'] / data['CapMrktCurUSD']) *100
+def aggregate_most_common(df):
+    df = df[['hash', 'owner']]
+    tmp = df[df['owner'].isna()]
+    tmp['owner'] = tmp['owner'].apply(randomString)
+    df = df.dropna()
+    df = df.append(tmp)
+    df = df.groupby(['hash'], as_index=False)['owner'].agg(pd.Series.mode) #https://stackoverflow.com/questions/15222754/groupby-pandas-dataframe-and-select-most-common-value
+    df['owner'] = df['owner'].apply(check_owner)
+    return df
 
-#filter
-final = data[data['dollar'] >= 10000000]
-#tmp = data[data['percent_marketcap'] >= 0.5]
-
-#export trainingset
-final.to_csv("transactions_10MIO.csv", index=False)
-
-
-
-
-#testing
-#a = max(data.date)
-#tnx.columns.values
-#tmp = tnx.head(1000)
-
-
-
-
-
-
-'''
-#https://stackoverflow.com/questions/15222754/groupby-pandas-dataframe-and-select-most-common-value
-tmp = tnx_labeled[['timestamp','btc', 'sender_name']]
-tmp = tmp.dropna()
-tmp = tmp.groupby(['timestamp','btc']).agg(lambda x:x.value_counts().index[0])
-tmp = tmp.groupby(['timestamp','btc'])['sender_name'].agg(pd.Series.mode).to_frame()
-'''
-
-
-
-'''
-#All Sender
-#All Receiver
-
-#Label Sender and Receiver
-#
-#Append and get unique Sender and Receiver
-#Get 
-
-Group by transaction_id (sum, category)
+def group_transactions(df, unique=False):
+    sender = df[['hash', 'sender_name']]
+    sender.rename(columns = {"sender_name" : 'owner'}, inplace = True) 
+    sender = aggregate_most_common(sender)
+    sender.rename(columns = {"owner" : 'sender_name2'}, inplace = True) 
+    
+    receiver = df[['hash', 'receiver_name']]
+    receiver.rename(columns = {"receiver_name" : 'owner'}, inplace = True) 
+    receiver = aggregate_most_common(receiver)
+    receiver.rename(columns = {"owner" : 'receiver_name2'}, inplace = True) 
+    
+    if unique:
+        df = df.drop_duplicates(subset='hash', keep='last')
+        
+    df_grouped = df.merge(sender, on="hash", how="inner")
+    df_grouped = df_grouped.merge(receiver, on="hash", how="inner")
+    return df_grouped
 
 
-'''
+def regroup(df):
+    tmp = df
+    sen = tmp[['sender', 'sender_name2']]
+    sen = sen[sen['sender_name2'] != 'unknown']
+    rec = tmp[['receiver', 'receiver_name2']]
+    rec = rec[rec['receiver_name2'] != 'unknown']
+    
+    rec.rename(columns = {"receiver_name2" : 'owner',
+                          "receiver" : 'address'}, inplace = True) 
+    
+    sen.rename(columns = {"sender_name2" : 'owner',
+                          "sender" : 'address'}, inplace = True) 
+    
+    allx = rec.append(sen)
+    allx = allx.drop_duplicates(keep='last') 
+    allx['category'] = 'Exchange'
+    return allx
+    #allx.to_csv('wallets_4.csv', mode='a', index=False)
+
+
+def get_missing_labels(df):
+    #get distinct addresses that have no label
+    sender = df[['sender', 'sender_name']]
+    sender = sender[sender['sender_name'].isna()]
+    sender.rename(columns = {"sender" : 'address'}, inplace = True) 
+    
+    receiver = df[['receiver', 'receiver_name']]
+    receiver = receiver[receiver['receiver_name'].isna()]
+    receiver.rename(columns = {"receiver" : 'address'}, inplace = True) 
+    
+    missing_labels = sender.append(receiver)
+    missing_labels = missing_labels[['address']]
+    missing_labels = missing_labels.drop_duplicates(keep='last')
+    
+    missing_labels.to_csv("missing_labels.csv", index = False)
+    
+    
+new_wallets = pd.DataFrame()
+
+for i in range(8):
+    filtered_transactions = merge_filter(new_wallets)
+    df_grouped = group_transactions(filtered_transactions)
+    new_wallets = new_wallets.append(regroup(df_grouped)).drop_duplicates(keep='last')
+    print(len(new_wallets))
+
+
+
+df_grouped.to_csv("transactions_10MIO.csv", index=False)
+
+df_unique = group_transactions(filtered_transactions, unique=True)
+df_unique = df_unique[['']]
+df_unique.to_csv("transactions_unique_10MIO.csv", index=False)
+
 
--- 2_data_exploration/blockchain_analysis.py
+++ 2_data_exploration/blockchain_analysis.py
@@ -25,3 +25,87 @@ res = tmp.groupby('monthly').count()
 
 res.plot()
 '''
+
+
+
+
+tmp = tmp[['date', 'dollar', 'sender_name2', 'receiver_name2']]
+exchange_exchange = tmp[(tmp['sender_name2'] != 'unknown') &  (tmp['receiver_name2'] != 'unknown')]
+other_exchange = tmp[(tmp['sender_name2'] == 'unknown') &  (tmp['receiver_name2'] != 'unknown')]
+exchange_other = tmp[(tmp['sender_name2'] != 'unknown') &  (tmp['receiver_name2'] == 'unknown')]
+other_other = tmp[(tmp['sender_name2'] == 'unknown') &  (tmp['receiver_name2'] == 'unknown')]
+
+
+
+date_start = '2017-11-01'
+date_end = '2018-04-01'
+all_days = pd.date_range(date_start, date_end, freq='D')
+
+def prepare_for_plot(df, category):
+    df = df.groupby('date').sum()
+    df = df[df['dollar'] < 50000000000] #remove outliers
+    df['category'] = category    
+    df = df.reindex(all_days)
+    #df.plot(figsize=(16, 8))
+    return df
+    
+exchange_exchange = prepare_for_plot(exchange_exchange, 'exchange')
+other_exchange = prepare_for_plot(other_exchange, 'to')
+exchange_other = prepare_for_plot(exchange_other, 'to')
+other_other = prepare_for_plot(other_other, 'to')
+all_tnx = prepare_for_plot(tmp, 'to')
+
+    
+price = pd.read_csv("data/btc.csv")
+price['date'] = pd.to_datetime(price['date'])
+price.set_index('date', inplace=True)
+price = price[['PriceUSD']]
+price = price.loc[date_start:date_end]
+price['return'] = price.pct_change(1) * 100
+
+
+
+
+
+
+plt.figure(figsize=(30,20))
+
+top1 = plt.subplot2grid((7,4), (2, 0), rowspan=1, colspan=4)
+top = plt.subplot2grid((7,4), (0, 0), rowspan=2, colspan=4)
+bottom = plt.subplot2grid((7,4), (3,0), rowspan=1, colspan=4)
+bottom2 = plt.subplot2grid((7,4), (4,0), rowspan=1, colspan=4)
+bottom3 = plt.subplot2grid((7,4), (5,0), rowspan=1, colspan=4)
+bottom4 = plt.subplot2grid((7,4), (6,0), rowspan=1, colspan=4)
+
+top1.plot(price.index, price['return']) #CMT.index gives the dates
+top1.axhline(y=0, color='r', linestyle='-')
+top.plot(price.index, price['PriceUSD']) #CMT.index gives the dates
+bottom.bar(other_exchange.index, other_exchange['dollar']) 
+bottom2.bar(exchange_other.index, exchange_other['dollar']) 
+bottom3.bar(other_other.index, other_other['dollar']) 
+bottom4.bar(exchange_exchange.index, exchange_exchange['dollar']) 
+ 
+
+# set the labels
+top.axes.get_xaxis().set_visible(False)
+top.set_title('Bitcoin Price')
+top.set_ylabel('Adj Closing Price')
+bottom.set_ylabel('Volume')
+
+
+'''
+fig, ax = plt.subplots(figsize=(20, 8)) 
+#exchange_exchange.plot(ax = ax,label='exchange') 
+other_exchange.plot(ax = ax) 
+exchange_other.plot(ax = ax) 
+#other_other.plot(ax = ax) 
+price.plot(ax = ax, secondary_y = True)
+ax.legend(["exchange", "to", 'from', 'other', 'price'])
+
+
+#all_txn = all_tnx.set_index('category', append=True).unstack().interpolate().plot(subplots=True)
+fig, ax= plt.subplots()
+for category in all_tnx['category'].unique():
+    all_tnx[all_tnx['category'] == category]['dollar'].plot.line(ax=ax, label=category)
+plt.legend()
+'''
